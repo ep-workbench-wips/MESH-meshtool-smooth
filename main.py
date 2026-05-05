6e86baeed0c0a7bc3a76cb2e18ad6bb25fe4480e
vinush-vigneswaran <vvignesw@ed.ac.uk>
2026-05-05 11:09:24 +0100

    smoothing changed

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ with tempfile.TemporaryDirectory() as temp_dir:
         "smooth", "mesh",
         f"-msh={input_mesh}",
         f"-outmsh={output_mesh}",
-        "-iter=100",
-        "-smth=0.8",
+        "-iter=500",
+        "-smth=0.7",
         "-tags=0",
         "-ifmt=carp_txt",
         "-ofmt=carp_txt"
